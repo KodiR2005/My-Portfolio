50113410b62886a8d85515cfc7baf3d83256fb9f
Cody <132841129+SJeysBusiness@users.noreply.github.com>
2023-05-21 02:29:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 }
+
+function openGmail() {
+    window.open("https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSDbhJNzrnZpPllQjbXvbCcpWnZlvlfhqccxvwZGXjnLvRSSwJdShLfMBGzPjnWNkZJzgRJF");
+  }
