621cdf404c10f4754d076388b0630fd33d578f4d
Cody <132841129+SJeysBusiness@users.noreply.github.com>
2023-05-21 02:48:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 }
 
-function openGmail() {
-    window.open("https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSDbhJNzrnZpPllQjbXvbCcpWnZlvlfhqccxvwZGXjnLvRSSwJdShLfMBGzPjnWNkZJzgRJF");
+function openComposeEmail() {
+    var emailAddress = "rc17.10.05@gmail.com"; // Replace with your email address
+    var url = "https://mail.google.com/mail/?view=cm&to=" + encodeURIComponent(emailAddress);
+    window.open(url);
   }
